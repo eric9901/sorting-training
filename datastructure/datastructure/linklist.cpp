4082da670ac956ec6915b078beaef7b11cb15911
Eric <mhkwok22-c@ad.cityu.edu.hk>
2018-08-08 16:08:19 +0800

    linklist(using temerate) added

--- datastructure/datastructure/linklist.cpp
+++ datastructure/datastructure/linklist.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include <string>
 using namespace std;
-template <T>;
-using T;
-class linknode {
+template <typename T>
+class node {
 public: T data;
+		node<T>* next,*head;
 };
+template <typename T>
+class linklist {
+private: node<T> *header, *tailer;
+     public:
+		 linklist():header(nullptr), tailer(nullptr) {};
+	 void add() {
+		 T indata;
+		 node<T>* current = new node<T>();
+		 cin >> indata;
+		 current->data = indata;
+		 if (header == nullptr&&tailer == nullptr)
+		 {
+			 header = current;
+			 tailer = current;
+		 }
+		 else {
+			 tailer->next = current;
+			 tailer = current;
+		 }
+	};
+	 void showall() {
+		 node<T>*current=header;
+		 while (current!=tailer) {
+			 cout << current->data << endl;
+			 current = current->next;
+		 }
+		 cout << current->data << endl;
+	 }
+};
+int main() {
+	linklist<int> list;
+	system("pause");
+	return 0;
+}
