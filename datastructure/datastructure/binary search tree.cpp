7895dd81551a481003d183f3922f327d45eb87c5
Eric <mhkwok22-c@ad.cityu.edu.hk>
2018-08-09 16:48:06 +0800

    comple binary tree added

--- datastructure/datastructure/binary search tree.cpp
+++ datastructure/datastructure/binary search tree.cpp
@@ -15,7 +15,7 @@ private:
 public:
 	tree() :roof(nullptr) {};
 	void add(int indata) {
-		node* addPosition;
+		node* addPosition=roof;
 		bool finded;
 
 		if (roof == nullptr) {
@@ -25,12 +25,34 @@ public:
 		}
 		else
 			while (!finded) {
-				if (indata >= roof->data) {
-		
-
-				};
+				if (indata >= addPosition->data) {
+					if (addPosition->secondSon == nullptr) {
+						addPosition->secondSon = new node();
+						addPosition->secondSon->parent = addPosition;
+						addPosition->data = indata;
+						finded = true;
+					}
+					else addPosition = addPosition->secondSon;
+				}
+				else {
+					if (addPosition->firstSon == nullptr) {
+						addPosition->firstSon = new node();
+						addPosition->firstSon->parent = addPosition;
+						addPosition->data = indata;
+						finded = true;
+					}
+					else addPosition = addPosition->firstSon;
+				}
 			}
 	}
-	void levelorder() {};
-	void basorder() {};
+	void TreeSearch() {};
 };
+int main() {
+	tree a;
+	a.add(20);
+	a.add(10);
+	a.add(30);
+	a.add(40);
+	cout >> a
+	return 0;
+}
